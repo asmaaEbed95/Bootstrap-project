4dbc64cdd96d464910096387da4ec6ccedb5f53a
asmaaEbed <asmaaebed95@gmail.com>
2019-06-22 21:45:54 +0200

    Add files via upload

--- js/plugins.js
+++ js/plugins.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $('.carousel').carousel({
         interval: 3000
     });
+    
+    //Show color option div when click on gear
+    $(".gear-cheack").click(function()
+      {
+        $(".color-option").fadeToggle();
+      });
 });
